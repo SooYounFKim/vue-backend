c3fafaeeea8d552a17dc0b536655972ac35b6eb8
SooYounFKim <sooyoun.f.kim@gmail.com>
2021-11-16 15:50:04 +0900

    데이터 및 api 생성

--- app.js
+++ app.js
@@ -38,4 +38,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+var moviesRouter = require('./routes/movies');
+app.use('/api/movies',moviesRouter)
+
 module.exports = app;
+
